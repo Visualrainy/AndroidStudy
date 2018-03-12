72864d41ad0d19c2f0782792db82c7fb2d523940
Peng Chen <pchen@thoughtworks.com>
2018-03-12 22:25:28 +0800

    init project

---
+++ app/src/main/java/tw/com/stencilproject/features/login/LoginActivity.kt
@@ -0,0 +1,23 @@
+package tw.com.stencilproject.features.login
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import tw.com.stencilproject.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val base = application.baseContext
+        test.text = "123";
+        val declaredField = base.javaClass.getDeclaredField("mPackageInfo")
+        declaredField.isAccessible = true
+        val packageInfo = declaredField.get(base)
+
+        Log.d("Testing: ", packageInfo.toString())
+    }
+}
